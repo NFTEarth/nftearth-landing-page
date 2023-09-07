ec52fa8c7bda3ab5ef9969ace8f7a215c6d6e922
Weston Nelson <dogooddowell@protonmail.com>
2023-09-07 12:29:01 -0500

    Update page.tsx
    Signed-off-by: Weston Nelson <dogooddowell@protonmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -60,7 +60,7 @@ export default function Home() {
       </div>
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-3 lg:text-left">
       <a
-          href="https://app.nftearth.exchange"
+          href="https://nftearth.exchange"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
